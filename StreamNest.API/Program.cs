1491dbeb7735cf8c1519fbe863fb34a3772000eb
Timmi-tech <tomiwatomzy66@gmail.com>
2025-08-13 14:09:08 +0100

    Fix project path in solution

--- StreamNest.API/Program.cs
+++ StreamNest.API/Program.cs
@@ -52,7 +52,12 @@ if (app.Environment.IsProduction())
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StreamNest.API v1"));
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "StreamNest.API v1");
+    c.RoutePrefix = string.Empty; // Serve at root URL
+});
+
 app.UseHttpsRedirection();
 
 app.UseRouting();
